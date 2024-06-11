12abbb07ec96a33b441285812764ff0b786e688b
Alexandre Delaboneta © <alexdelaboneta@gmail.com>
2024-06-11 17:21:11 -0300

    first commit

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const inputValue = document.querySelector('#inputValue');
 const btnValue = document.querySelector('#btnValue');
 const imgQrCode = document.querySelector('#imgQrCode');
+const btnCopy = document.querySelector('#btnCopy');
 const wrapper = document.querySelector('.wrapper');
 let valueDefault;
 
@@ -8,12 +9,27 @@ let valueDefault;
 
 btnValue.addEventListener('click', () => {
     let qrcodeValue = inputValue.value.trim();  
-    if(!qrcodeValue || qrcodeValue === valueDefault) return;
+    if (!qrcodeValue || qrcodeValue === valueDefault) return;
     valueDefault = qrcodeValue;
-    btnValue.innerText = 'Gerando QR Code...'
-    imgQrCode.src = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${valueDefault}`
+    btnValue.innerText = 'Gerando QR Code...';
+    imgQrCode.src = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${valueDefault}`;
     imgQrCode.addEventListener('load', () => {
         wrapper.classList.add('active');
-        btnValue.innerText = 'Gerar QRCode'
-    })
-})
+        btnValue.innerText = 'Gerar QRCode';
+        btnCopy.style.display = 'block'; // Mostrar o botão de copiar quando o QR code for gerado
+        btnCopy.style.marginTop = "5px";
+    });
+});
+
+btnCopy.addEventListener('click', () => {
+    fetch(imgQrCode.src)
+        .then(response => response.blob())
+        .then(blob => {
+            const item = new ClipboardItem({'image/png': blob});
+            navigator.clipboard.write([item]).then(() => {
+                alert('QR Code copiado para a área de transferência!');
+            }).catch(err => {
+                console.error('Falha ao copiar o QR Code:', err);
+            });
+        });
+});
